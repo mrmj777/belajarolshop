769906d5f0eda36585c7e4b07b4c312b5dbd5b91
Muhammad Jufri <muhammadjufri3@gmail.com>
2020-09-13 23:33:37 +0700

    Update collection Preview

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 const HatsPage = () => (
   <div>
     <h1>
@@ -13,7 +14,7 @@ const HatsPage = () => (
   return (
     <div >
       <Route exact  path='/' component={Homepage} />
-      <Route  path='/hats' component={HatsPage} />
+      <Route  path='/shop' component={ShopPage} />
     </div>
   );
 }
