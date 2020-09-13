e853bc50d09678999d9da7d56ae5608ea561d8e6
Muhammad Jufri <muhammadjufri3@gmail.com>
2020-09-13 19:39:19 +0700

    tambahin link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
-
-function App() {
+const HatsPage = () => (
+  <div>
+    <h1>
+      HATS PAGE
+    </h1>
+  </div>
+)
+ function App() {
   return (
     <div >
-      <Homepage />
+      <Route exact  path='/' component={Homepage} />
+      <Route  path='/hats' component={HatsPage} />
     </div>
   );
 }
